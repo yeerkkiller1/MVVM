0dd9156a2e13144fce830ef4036b23eb608217cf
Quentin Brooks <yeerkkiller1@gmail.com>
2015-01-25 11:12:40 -0800

    Flat directory structure

---
+++ updatebuild.bat
@@ -0,0 +1,19 @@
+cd ..
+
+cd ..
+git clone https://github.com/yeerkkiller1/Interfaces
+cd Interfaces
+chdir
+git pull
+
+cd ..
+git clone https://github.com/yeerkkiller1/Extensions
+cd Extensions
+chdir
+git pull
+
+cd ..
+git clone https://github.com/yeerkkiller1/MemoryManagement
+cd MemoryManagement
+chdir
+git pull
